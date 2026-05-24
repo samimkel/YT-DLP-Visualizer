56bfc52c5c3b65ae86fa3a9df45c6e3e7f65c38f
Samim KEL <samimkel00@gmail.com>
2026-05-24 19:05:40 +0300

    Initial Commit

---
+++ YT-DLP Visualizer/YT_DLP_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_DLP_VisualizerApp.swift
+//  YT-DLP Visualizer
+//
+//  Created by Samim KEL on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct YT_DLP_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
